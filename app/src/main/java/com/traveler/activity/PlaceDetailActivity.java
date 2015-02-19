44aad394cba1f95b16ddfe7349cb1f5e59617e59
vgrec <vgrec@winify.com>
2015-02-19 14:23:02 +0200

    Added fading Toolbar in place details screen.

--- app/src/main/java/com/traveler/activity/PlaceDetailActivity.java
+++ app/src/main/java/com/traveler/activity/PlaceDetailActivity.java
@@ -1,22 +1,65 @@
 package com.traveler.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ScrollView;
 
 import com.traveler.Extra;
+import com.traveler.R;
 import com.traveler.fragment.PlaceDetailFragment;
+import com.traveler.views.NotifyingScrollView;
 
-public class PlaceDetailActivity extends BaseActivity {
+public class PlaceDetailActivity extends BaseActivity implements NotifyingScrollView.OnScrollChangedListener {
+
+    private Drawable actionBarBackgroundDrawable;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
         setTitle("");
         String placeId = getIntent().getStringExtra(Extra.PLACE_ID);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        actionBarBackgroundDrawable = toolbar.getBackground();
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        NotifyingScrollView scrollView = (NotifyingScrollView) findViewById(R.id.scroll_view);
+        scrollView.setOnScrollChangedListener(this);
+        onScrollChanged(scrollView, 0, 0, 0, 0);
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, PlaceDetailFragment.newInstance(placeId))
+                    .add(R.id.content, PlaceDetailFragment.newInstance(placeId))
                     .commit();
         }
+    }
+
+
+    private void updateActionBarTransparency(float scrollRatio) {
+        int newAlpha = (int) (scrollRatio * 255);
+        actionBarBackgroundDrawable.setAlpha(newAlpha);
+        toolbar.setBackgroundDrawable(actionBarBackgroundDrawable);
+    }
+
+    @Override
+    public void onScrollChanged(ScrollView who, int l, int scrollPosition, int oldl, int oldt) {
+        int headerHeight = getResources().getDimensionPixelSize(R.dimen.header_image) - toolbar.getHeight();
+        float ratio = 0;
+        if (scrollPosition > 0 && headerHeight > 0)
+            ratio = (float) Math.min(Math.max(scrollPosition, 0), headerHeight) / headerHeight;
+
+        updateActionBarTransparency(ratio);
     }
 }
